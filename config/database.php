4f73af071f72b4d035ed1f8ba365b0b4e913147a
Ahmad Fauzi Abdilah <102431802+AhmadFauziAbdillah@users.noreply.github.com>
2024-11-20 20:56:51 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,12 +1,11 @@
 <?php
 $host = 'e96cun.stackhero-network.com';
-$port = '7872';
 $db   = 'dbscan';
 $user = 'root';
 $pass = 'MpHYBLAzid0n52I0u0EIYrp4v1Put1HO';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;port=port;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
